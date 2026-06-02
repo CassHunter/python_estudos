aad32e276c637021d5ef489a88b1e6e2e0f50d05
Petrus Menck <petrus.menck@unesp.br>
2026-06-02 14:23:32 -0300

    criação de execicios 3 até 10

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:")
+
+print(f'Hens {25+30/6:.1f}')
+print(f"Roosters, {100-25*3%4:.1f}")
+
+print("Now I will count the eggs:")
+
+print(f'{3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6:.1f}')
+
+print("Is it true that 3 + 2 < 5 -7")
+
+print(f'{3 + 2 < 5 -7:.1f}')
+print(f"What is 3 + 2?, {3+2:.1f}")
+print(f"What is 5 - 7?, {5-7:.1f}")
+
+print("Oh, that's why it's False")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5>=-2)
+print("Is it less or equal?", 5<=-2)
+
---
+++ ex5.py
@@ -0,0 +1,22 @@
+name = 'Zed A. Shaw'
+age = 35
+height = round(74 * 2.54) #polegadas convertido para cm
+weight = round(180 * 0.45) # libras convertido para kg
+eyes = 'Blue'
+teeth = "White"
+hair = "Brown"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} cm tall.")
+print(f"He's {weight} kg heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffe.")
+
+
+# essa linha é capciosa, tente escrever extamente com está
+total =  age + height + weight
+print(f"If I add {age}, {height:.2f}, and {weight:.1f} I get {total:.1f}.")
+
+
+
